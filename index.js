c5a37de4b65603b8e3da4379f3d8da15ff4bec2b
Tera Mind <teramind244@gmail.com>
2021-06-02 21:38:50 +0700

    Minor update

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const downloadFile = require("./src/helper/image-downloader");
 const getMeta = require("./src/helper/getMeta");
 const { images } = require("./src/data/data.json");
 
-const dest = "./downloaded";
+const dest = "downloaded";
 
 const main = async () => {
 	for (const imageUrl of images) {
 		const { fileName, extension } = getMeta(imageUrl);
+		console.log(`${fileName}.${extension}`);
+
 		await downloadFile(imageUrl, `./${dest}/${fileName}.${extension}`);
 	}
 }
--- src/helper/getMeta.js
+++ src/helper/getMeta.js
@@ -1,10 +1,18 @@
-const metaRegex = /\/(\w+).(\w{3,4}$)/;
+const metaRegex = /\/([^\/]+).(\w{3,4}$)/;
 
 const getMeta = (imageUrl) => {
 	const matches = metaRegex.exec(imageUrl);
+
+	if (matches) {
+		return {
+			fileName: matches[1],
+			extension: matches[2]
+		}
+	}
+	console.log(imageUrl);
 	return {
-		fileName: matches[1],
-		extension: matches[2]
+		fileName: "abc",
+		extension: "txt"
 	}
 }
 
